7df56abeea007efa6821deaa9a1ffbd80b27cf33
adrianopscl <adrianopscl@gmail.com>
2015-09-21 16:13:20 -0300

    correcoes para o firefox

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ $("#slider").owlCarousel({
       slideSpeed : 300,
       paginationSpeed : 400,
       singleItem:true
- 
-     
- 
   });
+
+$("ul.navbar-nav > li > a").on('click', function(event){
+	event.preventDefaut();
+	$("html, body").animate({scrollTop:$(this.hash).offset().top}, 1200);
+})
